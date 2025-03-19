95750b77d0847cdbae0b22043e94e6b384f85579
kalinduagathisi <kalinduagathisi@gmail.com>
2025-03-19 09:54:41 +0530

    implemented imgs and screen for toggling between light and dark modes

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
 
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true) // dismiss the keyboard once search/ go btn pressed
+        print(searchTextField.text!)
+    }
+    
+    // function to proceed when keyboards go btn pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    // inform weather, that textfield should dismiss the keyboard or not
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }
+        else {
+            textField.placeholder = "Type something.."
+            return false
+        }
+    }
+    
+    // clear the textfield once btn pressed
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
 }
 
---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Kalindu Agathisi on 2025-03-19.
+//  Copyright © 2025 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+//struct WeatherManager {
+//    
+//    let weatherUrl = 
+//}
